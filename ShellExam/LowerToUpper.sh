dcb62fe30e0fc2fb23d78cd43491a1dbd7388dad
Muhammed Shahin <117814937+ShahinMuhammed@users.noreply.github.com>
2023-09-10 17:11:10 +0530

    Update LowerToUpper.sh

--- ShellExam/LowerToUpper.sh
+++ ShellExam/LowerToUpper.sh
@@ -1,15 +1,21 @@
 read -p "Enter the file name : " fileName
+
+if [ ! -f $fileName ];
+	then echo "File not found"
+	exit 1
+fi
+
 echo "Converting from lower case to uppercase "
-
 tr '[ a-z ]' '[ A-Z ]' <$fileName
-echo "Converting from Uppercase to lowercase  "
 
+echo "Converting from Uppercase to lowercase  "
 tr '[ A-Z ]' '[ a-z ]' <$fileName
+
 echo "The number of line is : " $(wc --lines< $fileName)
 
 echo "The number of words is :" $(wc --words < $fileName)
 
-echo "The number of character is :" $(wc --char <$fileName)
+echo "The number of character is :$char" $(wc --char <$fileName)
 
 for i in $(cat $fileName)
 do
@@ -18,5 +24,5 @@ done
 
 echo "The file sorted in desending order"
 
-echo "$(sort "Temp")"
+echo "$(sort -r "Temp")"
 rm Temp
